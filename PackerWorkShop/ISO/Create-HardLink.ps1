e646bbed869fccb04b0d09cb9cf75534360a6958
Windom Wu <drw_08@hotmail.com>
2021-08-10 00:15:38 +0800

    add debian iso hardlink

--- PackerWorkShop/ISO/Create-HardLink.ps1
+++ PackerWorkShop/ISO/Create-HardLink.ps1
@@ -3,6 +3,7 @@ C:\Users\drw_0\Downloads\ISO\vyos-1.3.0-rc5-amd64.iso
 C:\Users\drw_0\Downloads\ISO\AlmaLinux-8.4-x86_64-dvd.iso
 C:\Users\drw_0\Downloads\ISO\ubuntu-20.04.2-live-server-amd64.iso
 C:\Users\drw_0\Downloads\ISO\alpine-virt-3.14.1-x86_64.iso
+C:\Users\drw_0\Downloads\ISO\debian-10.10.0-amd64-DVD-1.iso
 "@ -split "`r`n" | ForEach-Object {
     # $_.lastindexof("\")
     if (Test-Path $_) {
