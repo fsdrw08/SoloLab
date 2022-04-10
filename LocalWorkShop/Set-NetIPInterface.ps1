141aee53a2dcdeb3b68c4585fd3e4bfa38a5bff4
Windom <drw_08@hotmail.com>
2022-04-10 10:22:58 +0800

    update set-netipinterface

--- LocalWorkShop/Set-NetIPInterface.ps1
+++ LocalWorkShop/Set-NetIPInterface.ps1
@@ -1,4 +1,8 @@
 Get-NetIPInterface | Where-Object {$_.interfacealias -match "Internal Switch"} | ForEach-Object {
-    Set-NetIPInterface -InterfaceIndex $_.ifIndex -InterfaceMetric 9999 -Dhcp Disabled -AddressFamily IPv4,IPv6 -WhatIf
+    Set-NetIPInterface -InterfaceIndex $_.ifIndex -AddressFamily IPv4,IPv6 -InterfaceMetric 9999 -Dhcp Disabled -WhatIf
+    New-NetIPAddress -InterfaceIndex $_.ifIndex -AddressFamily IPv4 -IPAddress 192.168.255.2 -PrefixLength 24 -DefaultGateway 192.168.255.1
+    # Remove-NetIPAddress -InterfaceIndex 5 -AddressFamily IPv4  
+    # sudo Remove-NetRoute -InterfaceIndex 5 -Confirm:$false
+    # New-NetIPAddress -InterfaceIndex 5 -AddressFamily IPv4 -IPAddress 192.168.255.2 -PrefixLength 24 -DefaultGateway 192.168.255.1
 }
-Get-NetAdapter -Name "*internal switch*" | Set-NetAdapter -MacAddress "00-00-ba-be-fa-ce"
+# Get-NetAdapter -Name "*internal switch*" | Set-NetAdapter -MacAddress "00-00-ba-be-fa-ce"
