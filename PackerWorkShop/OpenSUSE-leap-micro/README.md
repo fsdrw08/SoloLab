09d2443144d62288992133b6419823988113e5c4
Windom <drw_08@hotmail.com>
2022-06-11 23:39:36 +0800

    add opensuse leap micro packer

---
+++ PackerWorkShop/OpenSUSE-leap-micro/README.md
@@ -0,0 +1,17 @@
+- check repo
+```
+zypper lr -U
+```
+
+- disable default repo
+```
+zypper mr -d openSUSE-MicroOS-5.2-OSS
+```
+- add tuna repo
+```shell
+sudo zypper ar -cfg 'https://mirrors.tuna.tsinghua.edu.cn/opensuse/distribution/leap-micro/5.2/product/repo/Leap-Micro-5.2-x86_64-Media/' 'Leap Micro Main Repository tuna'
+zypper mr -e 'Leap Micro Main Repository tuna'
+sudo zypper ar -cfg 'https://mirrors.tuna.tsinghua.edu.cn/opensuse/tumbleweed/repo/oss/' openSUSE-MicroOS-5.2-OSS-tuna
+sudo zypper ar -cfg 'https://mirrors.tuna.tsinghua.edu.cn/opensuse/update/openSUSE-stable/x86_64/' openSUSE-stable-tuna
+https://mirrors.tuna.tsinghua.edu.cn/opensuse/update/openSUSE-stable/x86_64/
+```
---
+++ PackerWorkShop/OpenSUSE-leap-micro/hv_opensuse-tumbleweed_base_vagrant.ps1
@@ -0,0 +1,64 @@
+#Verify the pre-request
+@"
+packer
+dos2unix
+"@ -split "`r`n" | ForEach-Object {
+  if (!(Get-Command $_)) {
+    [bool]$Ready = $false
+  }
+  $Ready
+}
+
+# Build images
+if ($Ready -ne $false) {
+  # Convert dos format to unix format
+  "dos2unix"
+  Get-ChildItem -Path $PSScriptRoot -Recurse -Filter "*.sh" `
+    | Select-Object -ExpandProperty VersionInfo `
+    | Select-Object -ExpandProperty filename `
+    | ForEach-Object {
+      #[io.file]::WriteAllText($_, ((Get-Content -Raw  $_) -replace "`r`n","`n"))
+      dos2unix $_
+    }
+
+  # Get Start Time
+  $startDTM = (Get-Date)
+  
+  # Variables
+  $template_file="$PSScriptRoot\OpenSUSE-leap-micro_base_g2_vagrant.pkr.hcl"
+  $var_file="$PSScriptRoot\variables_opensuse-tumbleweed.pkrvars.hcl"
+  $machine="opensuse-leap-micro"
+  $packer_log=0
+  
+  if ((Test-Path -Path "$template_file") -and (Test-Path -Path "$var_file")) {
+    Write-Output "Template and var file found"
+    Write-Output "Building: $machine"
+    $currentLocation = (Get-Location).Path
+    Set-Location $PSScriptRoot
+    try {
+      $env:PACKER_LOG=$packer_log
+      packer validate -var-file="$var_file" "$template_file"
+    }
+    catch {
+      Write-Output "Packer validation failed, exiting."
+      exit (-1)
+    }
+    try {
+      $env:PACKER_LOG=$packer_log
+      packer version
+      packer build --force -var-file="$var_file" "$template_file"
+    }
+    catch {
+      Write-Output "Packer build failed, exiting."
+      exit (-1)
+    }
+    Set-Location $currentLocation
+  }
+  else {
+    Write-Output "Template or var file not found - exiting"
+    exit (-1)
+  }
+}
+
+$endDTM = (Get-Date)
+Write-Host "[INFO]  - Elapsed Time: $(($endDTM-$startDTM).totalseconds) seconds" -ForegroundColor Yellow
