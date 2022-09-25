f2c7412ac142203ec6a3cd44722322fb3b8aabfd
Windom <drw_08@hotmail.com>
2022-09-25 18:25:55 +0800

    add repo source config for fedora vagrant box

---
+++ VagrantWorkShop/InfraSvc-Fedora/Set-CNRepo.sh
@@ -0,0 +1,8 @@
+# https://mirrors.ustc.edu.cn/help/fedora.html
+sudo sed -e 's|^metalink=|#metalink=|g' \
+         -e 's|^#baseurl=http://download.example/pub/fedora/linux|baseurl=https://mirrors.ustc.edu.cn/fedora|g' \
+         -i.bak \
+         /etc/yum.repos.d/fedora.repo \
+         /etc/yum.repos.d/fedora-modular.repo \
+         /etc/yum.repos.d/fedora-updates.repo \
+         /etc/yum.repos.d/fedora-updates-modular.repo
