67dc77f4ce2bd177697a4ae135f4c220ca7db6af
Windom <drw_08@hotmail.com>
2022-11-04 11:22:26 +0800

    update script

--- VagrantWorkShop/VyOS-New/Add-VMNetworkAdapter.ps1
+++ VagrantWorkShop/VyOS-New/Add-VMNetworkAdapter.ps1
@@ -5,4 +5,11 @@ param (
     $VMName
 )
 #powershell.exe -ExecutionPolicy bypass -command \"get-vm -Name `\"vyos*`\" | Add-VMNetworkAdapter -SwitchName 'Default Switch'\"
-Get-VM -Name $VMName | Add-VMNetworkAdapter -SwitchName 'Internal Switch'
+
+if (!(Get-VMNetworkAdapter -VMName $VMName | Where-Object {$_.SwitchName -eq "Default Switch"})) {
+    Get-VM -Name $VMName | Add-VMNetworkAdapter -SwitchName 'Default Switch'
+}
+
+if (!(Get-VMNetworkAdapter -VMName $VMName | Where-Object {$_.SwitchName -eq "Internal Switch"})) {
+    Get-VM -Name $VMName | Add-VMNetworkAdapter -SwitchName 'Internal Switch'
+}
