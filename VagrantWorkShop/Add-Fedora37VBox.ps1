549c31e246d4842f9e4fe9e6dfcd223bc03440dd
Windom <drw_08@hotmail.com>
2022-11-24 14:29:10 +0800

    add fedora37

---
+++ VagrantWorkShop/Add-Fedora37VBox.ps1
@@ -0,0 +1,1 @@
+vagrant box add FEDORA37-G2 $PSScriptRoot\..\PackerWorkShop\vbox\packer-fedora37-base-hv-g2.box
---
+++ VagrantWorkShop/Update-Fedora37VBox.ps1
@@ -0,0 +1,2 @@
+vagrant box remove FEDORA37-G2
+vagrant box add FEDORA37-G2 $PSScriptRoot\..\PackerWorkShop\vbox\packer-fedora37-base-hv-g2.box
