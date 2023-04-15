5b3c8874a9fe48165908b71a234e0716b3b650b6
windom.wu <drw_08@hotmail.com>
2023-04-15 12:44:08 +0800

    update vault unseal process

--- KubeWorkShop/Vault/unseal-vault.sh
+++ KubeWorkShop/Vault/unseal-vault.sh
@@ -1,8 +1,22 @@
+
+# https://nikhilism.com/post/2016/understanding-ansible-jinja2-default-filter/
+# https://www.freecodecamp.org/news/truthy-and-falsy-values-in-python/
 keys="
-{{ vault_unseal_keys_b64 }}
+{{ vault_unseal_keys_b64 | default('', true) }}
 "
-for i in $keys
-do
-    vault operator unseal $i
-    sleep 2
-done
+
+# https://blog.csdn.net/Jerry_1126/article/details/51835119
+length=$(echo -n $keys | wc -c)
+echo "length $length"
+
+if [ "$length" -le 2 ]; 
+then
+    echo "no unseal key"
+else
+    for i in $keys
+    do
+        # echo "it's $i"
+        vault operator unseal $i
+        # sleep 1
+    done
+fi
