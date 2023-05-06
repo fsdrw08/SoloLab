7aff1eb99ce738922da286e4c88bb7cb4b67bdfc
windom.wu <drw_08@hotmail.com>
2023-05-06 15:03:18 +0800

    try hyper-v terraform

---
+++ TerraformWorkShop/Hyper-V/VM_InfraSvc-Fedora/provider.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    hyperv = {
+      source  = "taliesins/hyperv"
+      version = "1.0.4"
+    }
+  }
+}
+
+# https://registry.terraform.io/providers/taliesins/hyperv/latest/docs
+provider "hyperv" {
+  user     = var.user
+  password = var.password
+  host     = var.host
+  port     = 5986
+  https    = true
+  insecure = true
+  use_ntlm = true
+  # tls_server_name = ""
+  # cacert_path     = ""
+  # cert_path       = ""
+  # key_path        = ""
+  script_path = "C:/Temp/terraform_%RAND%.cmd"
+  timeout     = "30s"
+}
