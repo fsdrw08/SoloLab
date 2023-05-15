2623419691c7252465c429c17d052a6feea97362
windom.wu <drw_08@hotmail.com>
2023-05-15 23:37:21 +0800

    change target to public download folder

--- PackerWorkShop/ISO/New-HardLink.ps1
+++ PackerWorkShop/ISO/New-HardLink.ps1
@@ -1,10 +1,10 @@
 @"
-$env:USERPROFILE\Downloads\ISO\vyos-1.3.2-amd64.iso
-$env:USERPROFILE\Downloads\ISO\alpine-virt-3.17.0-x86_64.iso
-$env:USERPROFILE\Downloads\ISO\debian-11.6.0-amd64-netinst.iso
-$env:USERPROFILE\Downloads\ISO\openSUSE-Leap-Micro-5.2-DVD-x86_64-Build38.1-Media.iso
-$env:USERPROFILE\Downloads\ISO\Fedora-Server-dvd-x86_64-38-1.6.iso
-$env:USERPROFILE\Downloads\ISO\Fedora-Server-netinst-x86_64-38-1.6.iso
+$env:PUBLIC\Downloads\ISO\vyos-1.3.2-amd64.iso
+$env:PUBLIC\Downloads\ISO\alpine-virt-3.17.0-x86_64.iso
+$env:PUBLIC\Downloads\ISO\debian-11.6.0-amd64-netinst.iso
+$env:PUBLIC\Downloads\ISO\openSUSE-Leap-Micro-5.2-DVD-x86_64-Build38.1-Media.iso
+$env:PUBLIC\Downloads\ISO\Fedora-Server-dvd-x86_64-38-1.6.iso
+$env:PUBLIC\Downloads\ISO\Fedora-Server-netinst-x86_64-38-1.6.iso
 "@ -split "`r`n" | ForEach-Object {
     # $_.lastindexof("\")
     if (Test-Path $_) {
--- PackerWorkShop/ISO/README.md
+++ PackerWorkShop/ISO/README.md
@@ -1,4 +1,4 @@
-1. Download related ISO to `$env:USERPROFILE\Downloads\ISO\`  
+1. Download related ISO to `$env:PUBLIC\Downloads\ISO\`  
    VYOS: https://vyos.net/get/snapshots/  
    Alpine Linux: https://www.alpinelinux.org/downloads/
 2. Run script [.\New-HardLink.bat](Create-HardLink.ps1) to create ISO hardlink from `$env:USERPROFILE\Downloads\ISO\` to this folder
