d3ec3809826060d8f0a7fa5a0ead68f44a96ef3d
windom.wu <drw_08@hotmail.com>
2023-06-07 18:01:34 +0800

    rename to main

--- TerraformWorkShop/Hyper-V/ISO-CloudInit_Nocloud2/main.tf
+++ TerraformWorkShop/Hyper-V/ISO-CloudInit_Nocloud2/main.tf
@@ -1,32 +1,3 @@
-data "cloudinit_config" "cloudinit" {
-  count = 3
-  part {
-    filename = "user-data"
-    content  = <<-EOT
-    #cloud-config
-    timezone: Asia/Shanghai
-    EOT
-  }
-
-  part {
-    filename = "user-data"
-    content  = <<-EOT
-    version: 2
-    ethernets:
-      eth0:
-        addresses:
-          - 192.168.255.1${count.index + 1}/255.255.255.0
-        gateway4: 192.168.255.1
-        nameservers:
-          addresses: 192.168.255.1
-    EOT
-  }
-}
-
-locals {
-  cloudinit = data.cloudinit_config.cloudinit
-}
-
 module "cloudinit_nocloud_iso" {
   source = "../modules/cloudinit_nocloud_iso2"
   count  = 3
@@ -41,7 +12,7 @@ module "cloudinit_nocloud_iso" {
         EOT
       },
       {
-        filename = "user-data"
+        filename = "network-config"
         content  = <<-EOT
         version: 2
         ethernets:
