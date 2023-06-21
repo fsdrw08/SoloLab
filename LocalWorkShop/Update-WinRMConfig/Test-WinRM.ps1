6eeee36b6168873a75f1461fa6af08e5239e005e
windom.wu <drw_08@hotmail.com>
2023-06-21 18:00:29 +0800

    improve logic

--- LocalWorkShop/Update-WinRMConfig/Test-WinRM.ps1
+++ LocalWorkShop/Update-WinRMConfig/Test-WinRM.ps1
@@ -2,7 +2,9 @@ $hostName=[System.Net.Dns]::GetHostName()
 $winrmPort = "5986"
 
 # Get the credentials of the machine
-$cred = Get-Credential
+if (!$cert) {
+    $cred = Get-Credential
+  }
 
 # Connect to the machine
 $soptions = New-PSSessionOption -SkipCACheck -SkipCNCheck
