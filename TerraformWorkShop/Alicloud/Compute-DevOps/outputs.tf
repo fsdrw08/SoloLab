158dbd74aaf03245aa8803ba64128c483d979cb9
windom.wu <drw_08@hotmail.com>
2023-06-29 20:45:53 +0800

    use private_key_pem instead

--- TerraformWorkShop/Alicloud/Compute-DevOps/outputs.tf
+++ TerraformWorkShop/Alicloud/Compute-DevOps/outputs.tf
@@ -1,15 +1,15 @@
 output "root_private_key" {
-  value     = tls_private_key.root.private_key_openssh
+  value     = tls_private_key.root.private_key_pem
   sensitive = true
 }
 
 output "admin_private_key" {
-  value     = tls_private_key.admin.private_key_openssh
+  value     = tls_private_key.admin.private_key_pem
   sensitive = true
 }
 
 output "podmgr_private_key" {
-  value     = tls_private_key.podmgr.private_key_openssh
+  value     = tls_private_key.podmgr.private_key_pem
   sensitive = true
 }
 
