d2dbe739da555bdd1cbf0bfd2505a5532aba4ce1
windom.wu <drw_08@hotmail.com>
2023-07-03 23:37:24 +0800

    add vpn gateway

--- TerraformWorkShop/Alicloud/Internet-DevOps/main.tf
+++ TerraformWorkShop/Alicloud/Internet-DevOps/main.tf
@@ -65,12 +65,41 @@ resource "alicloud_alidns_record" "alidnscheck" {
   value       = "c76842bdc6ac4e59bffacb1ad8df8f2d"
 }
 
-resource "alicloud_alidns_record" "r-proxy" {
-  domain_name = var.domain_name
-  rr          = "r-proxy"
-  remark      = "reverse proxy traefik mgmt web"
-  status      = "ENABLE"
-  ttl         = "600"
-  type        = "A"
-  value       = alicloud_eip_address.eip[0].ip_address
+# resource "alicloud_alidns_record" "r-proxy" {
+#   domain_name = var.domain_name
+#   rr          = "r-proxy"
+#   remark      = "reverse proxy traefik mgmt web"
+#   status      = "ENABLE"
+#   ttl         = "600"
+#   type        = "A"
+#   value       = alicloud_eip_address.eip[0].ip_address
+# }
+
+# https://github.com/jeremypedersen/terraformExamples/blob/f8e33f1266fbd64cc834001f8710bfd36e4fa7aa/abc/vpn-nat-demo/main.tf#L97
+resource "alicloud_vpn_gateway" "vpn" {
+  name                 = "DevOps_VPN_Gateway"
+  vpc_id               = data.alicloud_vpcs.vpc.vpcs.0.id
+  auto_propagate       = true
+  instance_charge_type = "PostPaid"
+  bandwidth            = "10"
+  enable_ssl           = true
+  ssl_connections      = "5"
+  vswitch_id           = data.alicloud_vswitches.vsw.vswitches.0.id
+  description          = "vpn for team access"
 }
+
+resource "alicloud_ssl_vpn_server" "vss" {
+  name           = "DevOps_VPN_SSL_Server"
+  vpn_gateway_id = alicloud_vpn_gateway.vpn.id
+  client_ip_pool = "10.0.0.0/27"
+  local_subnet   = data.alicloud_vswitches.vsw.vswitches.0.cidr_block
+  protocol       = "TCP"
+  cipher         = "AES-128-CBC"
+  port           = 1194
+  compress       = "false"
+}
+
+# resource "alicloud_ssl_vpn_client_cert" "vsc" {
+#   name              = "vpn_cert_test"
+#   ssl_vpn_server_id = alicloud_ssl_vpn_server.vss.id
+# }
