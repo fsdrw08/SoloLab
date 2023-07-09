e219b94fd99f53a02e11af07df59bb7edaba2162
Windom <drw_08@hotmail.com>
2023-07-09 23:53:55 +0800

    change disk category

--- TerraformWorkShop/Alicloud/DataDisk-DevOps/main.tf
+++ TerraformWorkShop/Alicloud/DataDisk-DevOps/main.tf
@@ -15,9 +15,19 @@ data "alicloud_vswitches" "vsw" {
 
 resource "alicloud_ecs_disk" "gitlab_data" {
   resource_group_id = data.alicloud_resource_manager_resource_groups.rg.groups.0.id
-  category          = "cloud_efficiency"
+  category          = "cloud_essd"
+  performance_level = "PL0"
   disk_name         = "DevOps_Disk-gitlab_data"
   description       = "This resource is managed by terraform"
   size              = "200"
   zone_id           = data.alicloud_vswitches.vsw.vswitches.0.zone_id
+}
+
+resource "alicloud_ecs_snapshot" "s-gitlab_data" {
+  resource_group_id = data.alicloud_resource_manager_resource_groups
+  category          = "standard"
+  description       = "This resource is managed by terraform"
+  disk_id           = alicloud_ecs_disk.gitlab_data.id
+  retention_days    = "20"
+  snapshot_name     = "DevOps_Disk-gitlab_data-snapshot"
 }
