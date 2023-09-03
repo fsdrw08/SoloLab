8f87825de3791ea0255ff60f61a419e1b82b7be9
Windom <drw_08@hotmail.com>
2023-09-03 23:54:44 +0800

    add script to stop hyper-v vm process

---
+++ LocalWorkShop/Stop-VMProcess/Stop-VMProcess.ps1
@@ -0,0 +1,5 @@
+# ref: https://www.nakivo.com/blog/how-to-disable-a-hyper-v-vm-stuck-in-the-starting-stopping-state/#:~:text=Method%203%3A%20Using%20PowerShell%20to%20kill%20the%20VM%20process
+$VMName="packer-opensuse-leap-g2"
+$VMGUID=get-vm $VMName | Select-Object -ExpandProperty Id  | Select-Object -ExpandProperty Guid 
+$VMWMProc = (Get-WmiObject Win32_Process | Where-Object {$_.Name -match 'VMWP' -and $_.CommandLine -match $VMGUID}) 
+Stop-Process ($VMWMProc.ProcessId) -Force
