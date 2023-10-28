8fb915827dd589769cfbcd27d1f7537d8f9618c7
windom.wu <drw_08@hotmail.com>
2023-10-29 00:16:43 +0800

    remove final config for new shellclash

--- TerraformWorkShop/Hyper-V/VM-VyOS-Test/main.tf
+++ TerraformWorkShop/Hyper-V/VM-VyOS-Test/main.tf
@@ -193,29 +193,6 @@ module "cloudinit_nocloud_iso" {
               commit
               save
 
-          # config after clash setup
-          - path: /tmp/finalConfig.sh
-            owner: root:vyattacfg
-            permissions: "0775"
-            content: |
-              #!/bin/vbash
-              # Ensure that we have the correct group or we'll corrupt the configuration
-              if [ "$(id -g -n)" != 'vyattacfg' ] ; then
-                  exec sg vyattacfg -c "/bin/vbash $(readlink -f $0) $@"
-              fi
-              source /opt/vyatta/etc/functions/script-template
-              configure
-              set nat destination rule 10 description 'CLASH FORWARD'
-              set nat destination rule 10 inbound-interface 'eth1'
-              set nat destination rule 10 protocol 'tcp_udp'
-              set nat destination rule 10 destination port '80,443'
-              set nat destination rule 10 source address '192.168.255.0/24'
-              set nat destination rule 10 translation address '192.168.255.1'
-              set nat destination rule 10 translation port '7892'
-              commit
-              save
-              exit
-
         EOT
       }
     ]
