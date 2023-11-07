48da9f0f993371aa33e7506f58177da08d8a2710
windom.wu <drw_08@hotmail.com>
2023-11-07 17:40:03 +0800

    hyper-v add core os related resource

---
+++ TerraformWorkShop/Hyper-V/VM-SvcDisc-CoreOS/provider.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    hyperv = {
+      source  = "taliesins/hyperv"
+      version = ">=1.0.4"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">=3.2.1"
+    }
+  }
+  backend "consul" {
+    address = "192.168.255.1:8500"
+    scheme  = "http"
+    path    = "tfstate/Dev-Fedora-VM"
+  }
+}
+
+# https://registry.terraform.io/providers/taliesins/hyperv/latest/docs
+provider "hyperv" {
+  user     = var.provider_hyperv.user
+  password = var.provider_hyperv.password
+  host     = var.provider_hyperv.host
+  port     = var.provider_hyperv.port
+  https    = true
+  insecure = true
+  use_ntlm = true
+  # tls_server_name = ""
+  # cacert_path     = ""
+  # cert_path       = ""
+  # key_path        = ""
+  script_path = "C:/Temp/terraform_%RAND%.cmd"
+  timeout     = "30s"
+}
