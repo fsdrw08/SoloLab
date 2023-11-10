3386809976382cc0dce12fb718135c4023f4b242
Windom <drw_08@hotmail.com>
2023-11-10 18:20:40 +0800

    update ignition config

--- TerraformWorkShop/Hyper-V/VM-SvcDisc-CoreOS/main.tf
+++ TerraformWorkShop/Hyper-V/VM-SvcDisc-CoreOS/main.tf
@@ -2,22 +2,84 @@ locals {
   count = 1
 }
 
-data "ignition_disk" "podmgr" {
+data "ignition_config" "ignition" {
+  count       = local.count
+  disks       = [data.ignition_disk.data.rendered]
+  filesystems = [data.ignition_filesystem.data.rendered]
+  systemd     = [data.ignition_systemd_unit.data.rendered]
+  directories = [data.ignition_directory.podmgr.rendered]
+  users = [
+    data.ignition_user.admin.rendered,
+    data.ignition_user.podmgr.rendered
+  ]
+  files = [
+    data.ignition_file.hostname.rendered,
+    data.ignition_file.eth0[count.index].rendered,
+    data.ignition_file.disable_dhcp.rendered
+  ]
+  links = [data.ignition_link.timezone]
+}
+
+# https://docs.fedoraproject.org/en-US/fedora-coreos/hostname/
+data "ignition_file" "hostname" {
+  count = local.count
+  path  = "/etc/hostname"
+  mode  = 420 # OCT: 0644
+  content {
+    content = local.count <= 1 ? "${var.vm_name}" : "${var.vm_name}${count.index + 1}" # 
+  }
+}
+
+# https://docs.fedoraproject.org/en-US/fedora-coreos/time-zone/
+data "ignition_link" "timezone" {
+  path   = "/etc/localtime"
+  target = "../usr/share/zoneinfo/Asia/Shanghai"
+}
+
+data "ignition_disk" "data" {
   device     = "/dev/sdb"
   wipe_table = false
   partition {
     number  = 1
-    label   = "podmgr"
+    label   = "data"
     sizemib = 0
   }
 }
 
-data "ignition_filesystem" "podmgr" {
-  device          = "/dev/disk/by-partlabel/podmgr"
+data "ignition_filesystem" "data" {
+  device          = "/dev/disk/by-partlabel/data"
   format          = "xfs"
   wipe_filesystem = false
-  label           = "podmgr"
+  label           = "data"
   path            = "/var/home/podmgr"
+}
+
+# the ignition provider does not provide filesystems.with_mount_unit like butane
+# https://coreos.github.io/butane/config-fcos-v1_5/
+# had to create the systemd mount unit manually
+# to debug, run journalctl --unit var-home-podmgr.mount -b-boot
+# https://github.com/getamis/terraform-ignition-etcd/blob/6526ce743d36f7950e097dabbff4ccfb41655de7/volume.tf#L28
+# https://github.com/meyskens/vagrant-coreos-baremetal/blob/5470c582fa42f499bc17eb501d3e592cf85caaf1/terraform/modules/ignition/systemd/files/data.mount.tpl
+# https://unix.stackexchange.com/questions/225401/how-to-see-full-log-from-systemctl-status-service/225407#225407
+data "ignition_systemd_unit" "data" {
+  # mind the unit name, The .mount file must be named based on the path (e.g. /var/mnt/data = var-mnt-data.mount)
+  # https://docs.fedoraproject.org/en-US/fedora-coreos/storage/#_configuring_nfs_mounts
+  name = "var-home-podmgr.mount"
+
+  content = <<EOT
+[Unit]
+Description=Mount data disk
+Before=local-fs.target
+
+[Mount]
+What=/dev/disk/by-partlabel/data
+Where=/var/home/podmgr
+Type=xfs
+DirectoryMode=0700
+
+[Install]
+RequiredBy=local-fs.target
+EOT
 }
 
 data "ignition_directory" "podmgr" {
@@ -27,18 +89,31 @@ data "ignition_directory" "podmgr" {
   gid  = 1001
 }
 
-data "ignition_user" "podmgr" {
-  name           = "podmgr"
-  uid            = 1001
-  home_dir       = "/home/podmgr/"
-  no_create_home = false
-  shell          = "/bin/bash"
-  password_hash  = "$y$j9T$I4IXP5reKRLKrkwuNjq071$yHlJulSZGzmyppGbdWHyFHw/D8Gl247J2J8P43UnQWA"
-  # ssh_authorized_keys = [
-  #   "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key"
-  # ]
+data "ignition_user" "admin" {
+  name = "admin"
+  uid  = 1002
+  groups = [
+    "wheel",
+    "sudo"
+  ]
+  # to gen password hash
+  # https://docs.fedoraproject.org/en-US/fedora-coreos/authentication/#_using_password_authentication
+  password_hash = "$y$j9T$cDLwsV9ODTV31Dt4SuVGa.$FU0eRT9jawPhIV3IV24W7obZ3PaJuBCVp7C9upDCcgD"
+  ssh_authorized_keys = [
+    "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key"
+  ]
 }
 
+data "ignition_user" "podmgr" {
+  name          = "podmgr"
+  uid           = 1001
+  password_hash = "$y$j9T$I4IXP5reKRLKrkwuNjq071$yHlJulSZGzmyppGbdWHyFHw/D8Gl247J2J8P43UnQWA"
+  ssh_authorized_keys = [
+    "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key"
+  ]
+}
+
+# https://docs.fedoraproject.org/en-US/fedora-coreos/sysconfig-network-configuration/#_disabling_automatic_configuration_of_ethernet_devices
 data "ignition_file" "disable_dhcp" {
   path      = "/etc/NetworkManager/conf.d/noauto.conf"
   mode      = 420
@@ -53,6 +128,7 @@ EOT
   }
 }
 
+# https://docs.fedoraproject.org/en-US/fedora-coreos/sysconfig-network-configuration/#_configuring_a_static_ip
 data "ignition_file" "eth0" {
   count     = local.count
   path      = "/etc/NetworkManager/system-connections/eth0.nmconnection"
@@ -72,19 +148,6 @@ gateway=192.168.255.1
 dns=192.168.255.1
 EOT
   }
-}
-
-data "ignition_config" "ignition" {
-  count       = local.count
-  disks       = [data.ignition_disk.podmgr.rendered]
-  filesystems = [data.ignition_filesystem.podmgr.rendered]
-  directories = [data.ignition_directory.podmgr.rendered]
-  users       = [data.ignition_user.podmgr.rendered]
-  files = [
-    data.ignition_file.eth0[count.index].rendered,
-    data.ignition_file.disable_dhcp.rendered
-  ]
-
 }
 
 # copy ignition file to remote
@@ -140,21 +203,43 @@ resource "null_resource" "remote" {
   }
 }
 
-resource "hyperv_vhd" "boot_disk" {
-  count = local.count
-  path = join("\\", [
-    var.vhd_dir,
-    local.count <= 1 ? "${var.vm_name}" : "${var.vm_name}${count.index + 1}",
-    join("", ["${var.vm_name}", ".vhdx"])
-    ]
-  )
-  source = var.source_disk
-}
-
 module "hyperv_machine_instance" {
-  source     = "../modules/hyperv_instance"
+  source     = "../modules/hyperv_instance2"
   depends_on = [null_resource.remote]
   count      = local.count
+
+  boot_disk = {
+    path = join("\\", [
+      var.vhd_dir,
+      local.count <= 1 ? "${var.vm_name}" : "${var.vm_name}${count.index + 1}",
+      join("", ["${var.vm_name}", ".vhdx"])
+      ]
+    )
+    source = var.source_disk
+  }
+
+  boot_disk_drive = [
+    {
+      controller_type     = "Scsi"
+      controller_number   = "0"
+      controller_location = "0"
+      path = join("\\", [
+        var.vhd_dir,
+        local.count <= 1 ? "${var.vm_name}" : "${var.vm_name}${count.index + 1}",
+        join("", ["${var.vm_name}", ".vhdx"])
+        ]
+      )
+    }
+  ]
+
+  additional_disk_drives = [
+    {
+      controller_type     = "Scsi"
+      controller_number   = "0"
+      controller_location = "2"
+      path                = local.count <= 1 ? var.data_disk_path : var.data_disk_path[count.index]
+    }
+  ]
 
   vm_instance = {
     name                 = local.count <= 1 ? var.vm_name : "${var.vm_name}${count.index + 1}"
@@ -212,21 +297,6 @@ module "hyperv_machine_instance" {
       }
     ]
 
-    hard_disk_drives = [
-      {
-        controller_type     = "Scsi"
-        controller_number   = "0"
-        controller_location = "0"
-        path                = hyperv_vhd.boot_disk[count.index].path
-        # path                = local.count <= 1 ? hyperv_vhd.boot_disk.path : hyperv_vhd.boot_disk[count.index].path
-      },
-      {
-        controller_type     = "Scsi"
-        controller_number   = "0"
-        controller_location = "1"
-        path                = local.count <= 1 ? var.data_disk_path : var.data_disk_path[count.index]
-      }
-    ]
   }
 }
 
