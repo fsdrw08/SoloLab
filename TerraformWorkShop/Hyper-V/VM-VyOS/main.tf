d8339bb1eae9f6174171a2f3e9a1ce9b5461cc69
windom.wu <drw_08@hotmail.com>
2023-11-22 00:05:05 +0800

    terraform-hyperv-vyos: add consul container script

--- TerraformWorkShop/Hyper-V/VM-VyOS/main.tf
+++ TerraformWorkShop/Hyper-V/VM-VyOS/main.tf
@@ -146,8 +146,8 @@ module "cloudinit_nocloud_iso" {
               chmod 777 /mnt/data/consul/config
               echo "Disk configuration complete"
 
-          # add container
-          - path: /tmp/Add-Container.sh
+          # add container Consul
+          - path: /tmp/Add-ContainerConsul.sh
             owner: root:vyattacfg
             permissions: '0775'
             content: |
@@ -162,7 +162,7 @@ module "cloudinit_nocloud_iso" {
               # Container networks
               set container network containers prefix '172.16.0.0/24'
               # consul
-              set container name consul network containers address 172.16.0.10
+              set container name consul network containers address 172.16.0.20
               set container name consul image docker.io/bitnami/consul:latest
               set container name consul port consul_rpc source 8300
               set container name consul port consul_rpc destination 8300
@@ -186,7 +186,39 @@ module "cloudinit_nocloud_iso" {
               set nat destination rule 20 protocol 'tcp_udp'
               set nat destination rule 20 destination port 8500
               set nat destination rule 20 source address 192.168.255.0/24
-              set nat destination rule 20 translation address 172.16.0.10
+              set nat destination rule 20 translation address 172.16.0.20
+
+              commit
+              save
+          # add container Cockpit-WS
+          - path: /tmp/Add-ContainerCockpitWS.sh
+            owner: root:vyattacfg
+            permissions: '0775'
+            content: |
+              #!/bin/vbash
+              # Ensure that we have the correct group or we'll corrupt the configuration
+              if [ "$(id -g -n)" != 'vyattacfg' ] ; then
+                  exec sg vyattacfg -c "/bin/vbash $(readlink -f $0) $@"
+              fi
+              source /opt/vyatta/etc/functions/script-template
+              run add container image quay.io/cockpit/ws:latest
+              configure
+              # Container networks
+              set container network containers prefix '172.16.0.0/24'
+              # consul
+              set container name cockpitws network containers address 172.16.0.30
+              set container name cockpitws image quay.io/cockpit/ws:latest
+              set container name cockpitws port cockpitws_http source 9090
+              set container name cockpitws port cockpitws_http destination 9090
+              set container name cockpitws environment 'TZ' value 'Asia/Shanghai'
+              # https://gist.github.com/peterkeen/97c566131af6f085628b5e4f1c4a8e1b
+              # https://www.reddit.com/r/vyos/comments/vkfuoo/dns_container_vyos/
+              set nat destination rule 30 description 'cockpitws forward'
+              set nat destination rule 30 inbound-interface 'eth1'
+              set nat destination rule 30 protocol 'tcp_udp'
+              set nat destination rule 30 destination port 9090
+              set nat destination rule 30 source address 192.168.255.0/24
+              set nat destination rule 30 translation address 172.16.0.30
 
               commit
               save
