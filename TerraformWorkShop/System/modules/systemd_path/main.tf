8f06699169821652f6f4f2740344f68cb4bde374
windom.wu <drw_08@hotmail.com>
2024-03-20 17:13:56 +0800

    terraform-system-systemd_path: create module for systemd path unit;

---
+++ TerraformWorkShop/System/modules/systemd_path/main.tf
@@ -0,0 +1,55 @@
+# ref: https://www.freedesktop.org/software/systemd/man/latest/systemd.path.html#:~:text=For%20each%20path,(see%20below).
+# persist systemd path unit file
+resource "system_file" "systemd_path" {
+  path    = var.systemd_path_unit.path
+  content = var.systemd_path_unit.content
+}
+
+# persist systemd service unit file which should trigger by systemd path unit
+resource "system_file" "systemd_service" {
+  path    = var.systemd_service_unit.path
+  content = var.systemd_service_unit.content
+}
+
+# resource "system_service_systemd" "traefik_restart_service" {
+#   depends_on = [
+#     null_resource.traefik_bin,
+#     system_file.traefik_config_static,
+#     system_file.traefik_restart_service
+#   ]
+#   # name = split(".", system_file.traefik_restart_service[each.key].basename)[0]
+#   name    = trimsuffix(system_file.traefik_restart_service.basename, ".service")
+#   enabled = var.enabled
+# }
+
+resource "null_resource" "systemd_path_control" {
+  depends_on = [
+    system_file.systemd_path,
+  ]
+  triggers = {
+    unit_name = system_file.systemd_path.basename
+    host      = var.vm_conn.host
+    port      = var.vm_conn.port
+    user      = var.vm_conn.user
+    password  = sensitive(var.vm_conn.password)
+  }
+  connection {
+    type     = "ssh"
+    host     = self.triggers.host
+    port     = self.triggers.port
+    user     = self.triggers.user
+    password = self.triggers.password
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sleep 5",
+      "sudo systemctl enable ${self.triggers.unit_name} --now",
+    ]
+  }
+  provisioner "remote-exec" {
+    when = destroy
+    inline = [
+      "sudo systemctl disable ${self.triggers.unit_name} --now",
+    ]
+  }
+}
---
+++ TerraformWorkShop/System/modules/systemd_path/variables.tf
@@ -0,0 +1,22 @@
+variable "vm_conn" {
+  type = object({
+    host     = string
+    port     = number
+    user     = string
+    password = string
+  })
+}
+
+variable "systemd_path_unit" {
+  type = object({
+    path    = string
+    content = string
+  })
+}
+
+variable "systemd_service_unit" {
+  type = object({
+    path    = string
+    content = string
+  })
+}
