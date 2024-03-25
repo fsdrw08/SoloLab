5ff42180c857b7b959b2e788a2a7e52970534319
Windom <drw_08@hotmail.com>
2024-03-25 16:26:53 +0800

    terraform-vault-consul: add consul auto_config related config; add user account for consul auto_config and assign related policy

---
+++ TerraformWorkShop/Vault/consul/auto_config/policy.tf
@@ -0,0 +1,55 @@
+resource "vault_policy" "consul_auto_config" {
+  name   = "consul-auto_config"
+  policy = <<-EOT
+  path "identity/oidc/token/consul_auto_config" {
+    capabilities = ["read"]
+  }
+  path "identity/entity/id" {
+    capabilities = ["list"]
+  }
+  path "identity/entity/id/{{identity.entity.id}}" {
+    capabilities = ["read", "update"]
+  }
+  EOT
+}
+
+resource "vault_identity_group" "consul_auto_config" {
+  name                       = "consul_auto_config"
+  type                       = "internal"
+  external_policies          = true
+  external_member_group_ids  = true
+  external_member_entity_ids = true
+}
+
+resource "vault_identity_group_policies" "consul_auto_config" {
+  policies = [
+    vault_policy.consul_auto_config.name
+  ]
+
+  exclusive = true
+
+  group_id = vault_identity_group.consul_auto_config.id
+}
+
+# data "vault_identity_group" "external" {
+#   group_name = "app-vault-admin"
+# }
+
+# resource "vault_identity_group_member_group_ids" "admin" {
+#   group_id = vault_identity_group.admin.id
+#   member_group_ids = [
+#     # data.vault_identity_group.external.group_id
+#   ]
+# }
+
+data "vault_identity_entity" "consul_auto_config" {
+  entity_name = "consul_auto_config"
+}
+
+resource "vault_identity_group_member_entity_ids" "consul_auto_config" {
+  depends_on = [data.vault_identity_entity.consul_auto_config]
+  group_id   = vault_identity_group.consul_auto_config.id
+  member_entity_ids = [
+    data.vault_identity_entity.consul_auto_config.entity_id
+  ]
+}
