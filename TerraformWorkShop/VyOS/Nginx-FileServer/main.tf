5adca2772c8b4ecaac3f41d97d8cc928fa614b4f
Windom <drw_08@hotmail.com>
2024-04-08 18:25:57 +0800

    terraform-vyos-nginx: add file server in vyos

---
+++ TerraformWorkShop/VyOS/Nginx-FileServer/main.tf
@@ -0,0 +1,89 @@
+resource "null_resource" "init" {
+  connection {
+    type     = "ssh"
+    host     = var.vm_conn.host
+    port     = var.vm_conn.port
+    user     = var.vm_conn.user
+    password = var.vm_conn.password
+  }
+  triggers = {
+    dirs        = "/mnt/data/offline"
+    chown_user  = "vyos"
+    chown_group = "users"
+    chown_dir   = "/mnt/data/offline"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      templatefile("${path.root}/nginx/init.sh", {
+        dirs        = self.triggers.dirs
+        chown_user  = self.triggers.chown_user
+        chown_group = self.triggers.chown_group
+        chown_dir   = self.triggers.chown_dir
+      })
+    ]
+  }
+}
+
+module "nginx_restart" {
+  source = "../../System/modules/systemd_path"
+  vm_conn = {
+    host     = var.vm_conn.host
+    port     = var.vm_conn.port
+    user     = var.vm_conn.user
+    password = var.vm_conn.password
+  }
+  systemd_path_unit = {
+    content = templatefile("${path.root}/nginx/restart.path", {
+      PathModified = []
+      PathChanged = [
+        "/etc/nginx/conf.d"
+      ]
+    })
+    path = "/lib/systemd/system/nginx_restart.path"
+  }
+  systemd_service_unit = {
+    content = templatefile("${path.root}/nginx/restart.service", {
+      AssertPathExists = "/lib/systemd/system/nginx.service"
+      target_service   = "nginx.service"
+    })
+    path = "/lib/systemd/system/nginx_restart.service"
+  }
+}
+
+resource "system_file" "config" {
+  depends_on = [
+    module.nginx_restart
+  ]
+  path = "/etc/nginx/conf.d/fileserver.conf"
+  content = templatefile("${path.module}/nginx/fileserver.conf", {
+    listen      = "192.168.255.1:8080"
+    server_name = "files.mgmt.sololab"
+    root        = "/mnt/data/offline"
+  })
+  user  = "vyos"
+  group = "users"
+  mode  = "644"
+}
+
+resource "vyos_static_host_mapping" "files" {
+  host = "files.mgmt.sololab"
+  ip   = "192.168.255.1"
+}
+
+# https://serverfault.com/questions/1078467/how-to-force-a-specific-routing-based-on-sni-in-haproxy/1078563#1078563
+resource "vyos_config_block_tree" "lb_svc_http_files" {
+  path = "load-balancing reverse-proxy service http rule 10"
+  configs = {
+    "domain-name" = "files.mgmt.sololab"
+    "set backend" = "files"
+  }
+}
+
+resource "vyos_config_block_tree" "lb_be_files" {
+  path = "load-balancing reverse-proxy backend files"
+  configs = {
+    "mode"                = "http"
+    "server vyos address" = "192.168.255.1"
+    "server vyos port"    = "8080"
+  }
+}
---
+++ TerraformWorkShop/VyOS/Nginx-FileServer/provider.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = ">=3.2.1"
+    }
+    system = {
+      source  = "neuspaces/system"
+      version = ">=0.4.0"
+    }
+    vyos = {
+      source  = "Foltik/vyos"
+      version = "0.3.3"
+    }
+  }
+}
+
+# https://registry.terraform.io/providers/neuspaces/system/latest/docs#usage-example
+provider "system" {
+  ssh {
+    host     = var.vm_conn.host
+    port     = var.vm_conn.port
+    user     = var.vm_conn.user
+    password = var.vm_conn.password
+  }
+  sudo = true
+}
+
+provider "vyos" {
+  url = "https://vyos-api.mgmt.sololab:8443"
+  key = "MY-HTTPS-API-PLAINTEXT-KEY"
+}
