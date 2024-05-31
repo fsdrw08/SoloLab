64e821f16b6e5adf1a14a7206644a755868a9d31
windom.wu <drw_08@hotmail.com>
2024-05-31 22:06:43 +0800

    vault: add liveness probe command; add post process

--- TerraformWorkShop/System/SvcDisc-Quadlet-vault/main.tf
+++ TerraformWorkShop/System/SvcDisc-Quadlet-vault/main.tf
@@ -137,3 +137,46 @@ resource "vyos_static_host_mapping" "host_mapping" {
   host = "vault.infra.sololab"
   ip   = "192.168.255.20"
 }
+
+locals {
+  post_process = {
+    New-VaultStaticToken = {
+      script_path = "./podman-vault/New-VaultStaticToken.sh"
+      vars = {
+        VAULT_OPERATOR_SECRETS_JSON_PATH = "/home/podmgr/.local/share/containers/storage/volumes/vault-pvc-unseal/_data/vault_operator_secrets"
+        VAULT_ADDR                       = "https://vault.infra.sololab:8200"
+        STATIC_TOKEN                     = "95eba8ed-f6fc-958a-f490-c7fd0eda5e9e"
+      }
+    }
+  }
+}
+
+resource "null_resource" "post_process" {
+  depends_on = [vyos_static_host_mapping.host_mapping]
+  for_each   = local.post_process
+  triggers = {
+    script_content = sha256(templatefile("${each.value.script_path}", "${each.value.vars}"))
+    host           = var.vm_conn.host
+    port           = var.vm_conn.port
+    user           = var.vm_conn.user
+    password       = sensitive(var.vm_conn.password)
+  }
+  connection {
+    type     = "ssh"
+    host     = self.triggers.host
+    port     = self.triggers.port
+    user     = self.triggers.user
+    password = self.triggers.password
+  }
+  provisioner "remote-exec" {
+    inline = [
+      templatefile("${each.value.script_path}", "${each.value.vars}")
+    ]
+  }
+  # provisioner "remote-exec" {
+  #   when = destroy
+  #   inline = [
+  #     "sudo rm -f ${self.triggers.file_source}/consul",
+  #   ]
+  # }
+}
---
+++ TerraformWorkShop/System/SvcDisc-Quadlet-vault/podman-vault/New-VaultStaticToken.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# https://stackoverflow.com/questions/19331497/set-environment-variables-from-file-of-key-value-pairs
+VAULT_OPERATOR_SECRETS_JSON_PATH=${VAULT_OPERATOR_SECRETS_JSON_PATH}
+VAULT_ADDR=${VAULT_ADDR}
+STATIC_TOKEN=${STATIC_TOKEN}
+
+# https://github.com/Indellient/vault-habitat/blob/2a010ee30b2639e65d3df5ad05df47c07c0eec55/vault/hooks/run#L49
+function wait_started {
+  counter=0
+  until STATUS=$(curl -k -X GET "$VAULT_ADDR/v1/sys/health" 2>&1); [ $? -ne 1 ]
+  do
+    if [ $counter -lt 20 ]; then
+      echo "Waiting for vault to come up, $(($counter*5))s passed"
+      sleep 5
+      ((counter++))
+    else
+      echo "check vault stauts manually"
+      exit 1
+    fi
+  done
+  echo "Vault is started"
+}
+
+function wait_ready {
+    retried=1
+    # https://github.com/homedepot/spingo/blob/d5f418cc438ec176a219258f816178028cec5394/scripts/initial-setup.sh#L305
+    status=$(curl -s -k -X GET "$VAULT_ADDR/v1/sys/health" | jq -r '. | select(.initialized == true and .sealed == false) | .initialized')
+    until [ "$status" = "true" ]
+    do
+        if [ $retried -lt 3 ]; then
+            echo "[$retried] Vault not initialized nor unseal "
+            ((retried++))
+            sleep 5
+            status=$(curl -s -k -X GET "$VAULT_ADDR/v1/sys/health" | jq -r '. | select(.initialized == true and .sealed == false) | .initialized')
+        else
+            echo "check vault stauts manually"
+            exit 1
+        fi
+    done
+    echo "Vault is initialized and unsealed"
+}
+
+function authenticate {
+    # Authenticate Vault
+    printf "Authenticating Vault...\n"
+    VAULT_OPERATOR_SECRETS=$(cat $VAULT_OPERATOR_SECRETS_JSON_PATH)
+    VAULT_TOKEN=$(echo $VAULT_OPERATOR_SECRETS | jq -r .root_token)
+    export VAULT_TOKEN=$VAULT_TOKEN
+}
+
+# https://github.com/Kehrlann/concourse-demo/blob/ecf3b68b5da125b6c14f5e04642dc0aa835250e2/demo/infrastructure/setup-vault.sh#L36-L40
+function create_token {
+    RESULT=$(curl -s -k --request POST \
+    -H "X-Vault-Token: $VAULT_TOKEN" \
+    --data "{ \"token\": \"$STATIC_TOKEN\" }" \
+    $VAULT_ADDR/v1/auth/token/lookup | jq -r .data.id)
+
+    if [ $RESULT != $STATIC_TOKEN ]; then
+        echo "New static token"
+        curl -s -k --request POST \
+            --header "X-Vault-Token: $VAULT_TOKEN" \
+            --data "{  \"policies\": [\"root\"],  \"id\": \"$STATIC_TOKEN\" }" \
+            $VAULT_ADDR/v1/auth/token/create
+    else
+        echo $RESULT
+    fi
+}
+
+function unauthenticate {
+    # Unauthenticate Vault
+    printf "Unauthenticating Vault...\n"
+    unset VAULT_TOKEN
+    printf "Unauthenticated Vault.\n"
+}
+
+wait_started
+wait_ready
+authenticate
+create_token
+unauthenticate
