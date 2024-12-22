28530591142c9ba71ea9df1d83fef5f6810e05f0
fsdrw08 <drw_08@hotmail.com>
2024-12-22 15:19:28 +0800

    powerdns: add etcd record

--- TerraformWorkShop/PowerDNS/day1/main.tf
+++ TerraformWorkShop/PowerDNS/day1/main.tf
@@ -1,8 +1,19 @@
 resource "powerdns_zone" "zone" {
-  name = "day1.sololab."
-  kind = "Native"
+  name         = "day1.sololab."
+  kind         = "Native"
+  soa_edit_api = "DEFAULT"
   nameservers = [
     "ns1.day1.sololab."
+  ]
+}
+
+resource "powerdns_record" "SOA" {
+  zone = powerdns_zone.zone.name
+  name = "day1.sololab."
+  type = "SOA"
+  ttl  = 86400
+  records = [
+    "ns1.day1.sololab. day1.sololab. 2024102501 3600 600 1814400 7200"
   ]
 }
 
@@ -25,5 +36,15 @@ resource "powerdns_record" "etcd_SRV" {
     "0 10 2380 etcd-0.day1.sololab.",
     # "0 10 2380 etcd-1.day1.sololab.",
     # "0 10 2380 etcd-2.day1.sololab.",
+  ]
+}
+
+resource "powerdns_record" "etcd_A" {
+  zone = powerdns_zone.zone.name
+  name = "etcd-0.day1.sololab."
+  type = "A"
+  ttl  = 86400
+  records = [
+    "192.168.255.20"
   ]
 }
--- TerraformWorkShop/PowerDNS/day1/terraform.tfvars
+++ TerraformWorkShop/PowerDNS/day1/terraform.tfvars
@@ -1,4 +1,4 @@
 pdns = {
   api_key    = "powerdns"
-  server_url = "https://pdns.day0.sololab:8081"
+  server_url = "https://pdns.day0.sololab"
 }
