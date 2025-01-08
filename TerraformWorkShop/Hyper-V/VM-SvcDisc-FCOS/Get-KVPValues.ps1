8e9028d88012e91b51e605bb52a3c427f1a03008
fsdrw08 <drw_08@hotmail.com>
2025-01-08 20:00:20 +0800

    fcos: add powershell script to fetch ignition data

--- TerraformWorkShop/Hyper-V/VM-SvcDisc-FCOS/Get-KVPValues.ps1
+++ TerraformWorkShop/Hyper-V/VM-SvcDisc-FCOS/Get-KVPValues.ps1
@@ -1,32 +1,84 @@
-function GetKVP {
-    param($VMName, $key)
-
-    $VmMgmt = Get-WmiObject -Namespace "Root\Virtualization\V2" -Class Msvm_VirtualSystemManagementService #Get-WmiObject -Namespace root\virtualization\v2 -Class Msvm_VirtualSystemManagementService
-    $Vm = Get-WmiObject -Namespace "root\virtualization\v2" -Class Msvm_ComputerSystem| Where-Object {$_.ElementName -eq $VMName }  #Get-WmiObject -Namespace root\virtualization\v2 -Class Msvm_ComputerSystem -Filter {ElementName='TEST-APP38'}     # has to be same as $VMName
-
-    $n = $vm.GetRelated("Msvm_KvpExchangeComponent").GuestIntrinsicExchangeItems
-    $n = $vm.GetRelated("Msvm_KvpExchangeComponent").GuestExchangeItems
-
-    $n = $vm.GetRelated("Msvm_KvpExchangeComponent").GetRelated('Msvm_KvpExchangeComponentSettingData').HostExchangeItems
-
-    $n | ForEach-Object {
-        $GuestExchangeItemXml = ([XML]$_).SelectSingleNode(`
-            "/INSTANCE/PROPERTY[@NAME='Name']/VALUE[child::text()='$key']")
-
-        if ($null -ne $GuestExchangeItemXml)
-        {
-            $val = $GuestExchangeItemXml.SelectSingleNode( `
-                "/INSTANCE/PROPERTY[@NAME='Data']/VALUE/child::text()").Value
-                $val
-                Return
-        }
+# https://github.com/NathanielArnoldR2/KVPTools/blob/022e103c61f78a2e2de5e01849c45d4d11216ea7/KVPTools.psm1#L326
+function Get-KvpValue {
+    [CmdletBinding(
+      PositionalBinding = $false
+    )]
+    param(
+      [Parameter(
+        Mandatory = $true
+      )]
+      [guid]
+      $VMId,
+  
+      [Parameter(
+        Mandatory = $true
+      )]
+      [ValidateSet("Guest", "GuestIntrinsic", "Host")]
+      [String]
+      $Origin,
+  
+      [Parameter(
+        Mandatory = $true
+      )]
+      [String]
+      $Name
+    )
+  
+    $kvpObject = Get-CimInstance -Namespace root\virtualization\v2 `
+                                 -ClassName Msvm_ComputerSystem `
+                                 -Filter "Name='$VMId'" |
+                   Get-CimAssociatedInstance -ResultClassName Msvm_KvpExchangeComponent
+  
+    if ($null -eq $kvpObject) {
+      throw "The KVP store for this VM could not be queried." # Let the calling code determine severity of failure.
     }
-}
+  
+    if ($Origin -eq "Guest") {
+      $items = $kvpObject.GuestExchangeItems
+    }
+    elseif ($Origin -eq "GuestIntrinsic") {
+      $items = $kvpObject.GuestIntrinsicExchangeItems
+    }
+    elseif ($Origin -eq "Host") {
+      $kvpObject = $kvpObject |
+                     Get-CimAssociatedInstance -ResultClassName Msvm_KvpExchangeComponentSettingData
+  
+      if ($null -eq $kvpObject) {
+        throw "The Host KVP store for this VM could not be queried." # Let the calling code determine severity of failure.
+      }
+  
+      $items = $kvpObject.HostExchangeItems
+    }
+  
+    $item = $items |
+              ForEach-Object {[xml]$_} |
+              ForEach-Object INSTANCE |
+              Where-Object {
+                $_ |
+                  ForEach-Object PROPERTY |
+                  Where-Object Name -eq $Name |
+                  ForEach-Object Value |
+                  ForEach-Object Equals $Name
+              }
+  
+    if ($null -eq $item) {
+      return
+    }
+  
+    $item |
+      ForEach-Object PROPERTY |
+      Where-Object Name -eq Data |
+      ForEach-Object Value
+  }
 
-$vmName = "SvcDisc-FCOS"
-$vm=Get-WmiObject -Namespace "root\virtualization\v2" -Class Msvm_ComputerSystem| Where-Object {$_.ElementName -eq $vmName }
-GetKVP -VMName "SvcDisc-FCOS" -key "ignition.config.0"
+$VMId = Get-VM -Name "SvcDisc-FCOS" | Select-Object -ExpandProperty Id
+Get-KvpValue -VMId $VMId -Origin "Host" -Name "Name"
 
-# https://github.com/NathanielArnoldR2/KVPTools/blob/022e103c61f78a2e2de5e01849c45d4d11216ea7/KVPTools.psm1#L49
-Get-CimInstance -Namespace root\virtualization\v2 -ClassName Msvm_ComputerSystem -Filter "ElementName='$vmName'" `
-    | Get-CimAssociatedInstance -ResultClassName Msvm_KvpExchangeDataItem
+Get-CimInstance -Namespace root\virtualization\v2 `
+        -ClassName Msvm_ComputerSystem `
+        -Filter "Name='$VMId'" | `
+        Get-CimAssociatedInstance -ResultClassName Msvm_KvpExchangeComponent | `
+        Get-CimAssociatedInstance -ResultClassName Msvm_KvpExchangeComponentSettingData | `
+    Select-Object -ExpandProperty HostExchangeItems |
+    ForEach-Object {[xml]$_} |
+    ForEach-Object INSTANCE | Select-Object -ExpandProperty PROPERTY
