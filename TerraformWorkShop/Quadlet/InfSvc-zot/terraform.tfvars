9719d312a9c7e737f507b9a2b70690d0194de278
fsdrw08 <drw_08@hotmail.com>
2025-03-10 23:25:22 +0800

    zot-podman-kube: create tf resource

---
+++ TerraformWorkShop/Quadlet/InfSvc-zot/terraform.tfvars
@@ -0,0 +1,53 @@
+prov_remote = {
+  host     = "192.168.255.20"
+  port     = 22
+  user     = "podmgr"
+  password = "podmgr"
+}
+
+podman_kube = {
+  helm = {
+    name       = "zot"
+    chart      = "../../../HelmWorkShop/helm-charts/charts/zot"
+    value_file = "./podman-zot/values-sololab.yaml"
+    tls_value_sets = {
+      name = {
+        ca          = "zot.tls.contents.\"ca\\.crt\""
+        cert        = "zot.tls.contents.\"server\\.crt\""
+        private_key = "zot.tls.contents.\"server\\.key\""
+      }
+      value_ref = {
+        tfstate = {
+          backend = {
+            type = "local"
+            config = {
+              path = "../../TLS/RootCA/terraform.tfstate"
+            }
+          }
+          cert_name = "zot"
+        }
+      }
+    }
+  }
+  manifest_dest_path = "/home/podmgr/.config/containers/systemd/zot-aio.yaml"
+}
+
+podman_quadlet = {
+  files = [
+    {
+      template = "./podman-zot/zot-container.kube"
+      vars = {
+        Description   = "OCI-native container image registry, simplified"
+        Documentation = "https://zotregistry.dev/latest/admin-guide/admin-configuration/"
+        yaml          = "zot-aio.yaml"
+        PodmanArgs    = "--tls-verify=false"
+        KubeDownForce = "true"
+      }
+      dir = "/home/podmgr/.config/containers/systemd"
+    }
+  ]
+  service = {
+    name   = "zot-container"
+    status = "start"
+  }
+}
---
+++ TerraformWorkShop/Quadlet/InfSvc-zot/variables.tf
@@ -0,0 +1,115 @@
+variable "prov_remote" {
+  type = object({
+    host     = string
+    port     = number
+    user     = string
+    password = string
+  })
+}
+
+# variable "prov_vault" {
+#   type = object({
+#     schema          = string
+#     address         = string
+#     token           = string
+#     skip_tls_verify = bool
+#   })
+# }
+
+variable "podman_kube" {
+  type = object({
+    helm = object({
+      name       = string
+      chart      = string
+      value_file = string
+      value_sets = optional(
+        list(
+          object({
+            name                = string
+            value_string        = optional(string, null)
+            value_template_path = optional(string, null)
+            value_template_vars = optional(map(string), null)
+          })
+        ), null
+      )
+      tls_value_sets = optional(
+        object({
+          name = object({
+            ca          = string
+            cert        = string
+            private_key = string
+          })
+          value_ref = object({
+            vault_kvv2 = optional(
+              object({
+                mount = string
+                name  = string
+                data_key = object({
+                  ca          = string
+                  cert        = string
+                  private_key = string
+                })
+              }),
+              null
+            )
+            tfstate = optional(
+              object({
+                backend = object({
+                  type   = string
+                  config = map(string)
+                })
+                cert_name = string
+              }),
+              null
+            )
+          })
+        }),
+        null
+      )
+    })
+    manifest_dest_path = string
+  })
+}
+
+variable "podman_quadlet" {
+  type = object({
+    files = list(object({
+      template = string
+      vars     = map(string)
+      dir      = string
+    }))
+    service = optional(
+      object({
+        name   = string
+        status = string
+      }),
+      null
+    )
+  })
+}
+
+# variable "prov_pdns" {
+#   type = object({
+#     api_key        = string
+#     server_url     = string
+#     insecure_https = optional(bool, null)
+#   })
+# }
+
+# variable "dns_record" {
+#   type = object({
+#     zone    = string
+#     name    = string
+#     type    = string
+#     ttl     = number
+#     records = list(string)
+#   })
+# }
+
+variable "post_process" {
+  type = map(object({
+    script_path = string
+    vars        = map(string)
+  }))
+  default = null
+}
