4c763e55d17dc166bda58fb65ac0b942629df1fb
fsdrw08 <drw_08@hotmail.com>
2025-05-19 23:43:53 +0800

    traefik-deploy-podman: upgrade image version

--- TerraformWorkShop/VyOS/Container-Zot/Sync-OCIImage.ps1
+++ TerraformWorkShop/VyOS/Container-Zot/Sync-OCIImage.ps1
@@ -162,10 +162,10 @@ $syncList = @"
     },
     {
         "publicRegistry": "docker.io",
-        "publicRepo": "library/traefik:v3.3.4",
-        "archive": "docker.io_library_traefik_v3.3.4.tar",
+        "publicRepo": "library/traefik:v3.4.0",
+        "archive": "docker.io_library_traefik_v3.4.0.tar",
         "privateRegistry": "192.168.255.10:5000",
-        "privateRepo": "library/traefik:v3.3.4",
+        "privateRepo": "library/traefik:v3.4.0",
         "description": "https://hub.docker.com/r/library/traefik/tags",
     },
     {
