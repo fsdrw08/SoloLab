792dcb89ac423c1a5e5209174b670b7775814cfb
fsdrw08 <drw_08@hotmail.com>
2025-06-21 13:06:55 +0800

    vault-ops-consul-acl_token: update variable to match module

--- TerraformWorkShop/Vault/consul/ACL_Token/main.tf
+++ TerraformWorkShop/Vault/consul/ACL_Token/main.tf
@@ -26,14 +26,16 @@ module "consul_jwt_auth_policy_bindings" {
   source = "../../../modules/vault-policy_binding"
   policy_bindings = [
     for role in var.consul_roles : {
-      policy_name     = "consul-acl_token-${role.name}"
-      policy_content  = <<-EOT
+      policy_name    = "consul-acl_token-${role.name}"
+      policy_content = <<-EOT
       path "consul/creds/${role.name}" {
         capabilities = ["read"]
       }
       EOT
-      policy_group    = "Policy-Consul-Creds_${role.name}"
-      external_groups = role.groups_binding
+      group_binding = {
+        policy_group    = "Policy-Consul-Creds_${role.name}"
+        external_groups = role.groups_binding
+      }
     }
   ]
 }
