f0549141a2d3f175948138a51b98057162106c01
fsdrw08 <drw_08@hotmail.com>
2025-08-07 17:12:54 +0800

    OCI-ops: move image sync script to dedicate folder, split images to files by vm host

---
+++ LocalWorkShop/Sync-OCIImage/Sync-OCIImage.ps1
@@ -0,0 +1,37 @@
+$syncList = Get-Content .\Day0.jsonc
+$syncList = Get-Content .\Day1.jsonc
+
+$localDir="$env:PUBLIC/Downloads/containers"
+# $localDir="C:/Users/Public/Downloads/containers"
+# $localDir="D:/Users/Public/Downloads/containers"
+# Test-Path -Path $localDir
+
+$proxy="127.0.0.1:7890"
+# $proxy="192.168.255.1:7890"
+$env:HTTP_PROXY=$proxy; $env:HTTPS_PROXY=$proxy
+
+$syncList | ConvertFrom-Json | ForEach-Object {
+    if (-not (Test-Path -Path $localDir/$($_.archive))) {
+        Write-Host "Download docker image then convert to OCI archive $localDir/$($_.archive)"
+        skopeo copy --insecure-policy `
+            --override-os=linux `
+            --override-arch=amd64 `
+            docker://$($_.publicRegistry)/$($_.publicRepo) `
+            oci-archive:$localDir/$($_.archive)
+    }
+}
+
+$proxy=""
+$env:HTTP_PROXY=$proxy; $env:HTTPS_PROXY=$proxy
+
+$syncList | ConvertFrom-Json | ForEach-Object {
+    if (Test-Path -Path $localDir/$($_.archive)) {
+    Write-Host "Upload OCI image OCI archive $localDir/$($_.archive) to $($_.privateRegistry)/$($_.privateRepo)"
+    skopeo copy --insecure-policy `
+        --dest-tls-verify=false `
+        --dest-creds="admin:P@ssw0rd" `
+        oci-archive:$localDir/$($_.archive) `
+        docker://$($_.privateRegistry)/$($_.privateRepo)
+    }
+    Start-Sleep -Seconds 1
+}
