38a23ec29b388bb1f7b6c9d32c67066e2b163405
Windom.WU <drw_08@hotmail.com>
2025-08-11 18:00:15 +0800

    local: add script to sync binaries to dufs

---
+++ LocalWorkShop/Sync-Binaries/Sync-Binaries.ps1
@@ -0,0 +1,43 @@
+$repoDir = git rev-parse --show-toplevel
+$syncList = Get-Content -Path (Join-Path -Path $repoDir -ChildPath "LocalWorkShop\Sync-Binaries\Day2.jsonc")
+# $syncList = Get-Content -Path (Join-Path -Path $repoDir -ChildPath "LocalWorkShop\Sync-Binaries\Day1.jsonc")
+
+$localDir="$env:PUBLIC/Downloads/bin"
+# $localDir="C:/Users/Public/Downloads/containers"
+# $localDir="D:/Users/Public/Downloads/containers"
+if (-not (Test-Path -Path $localDir)) {
+    New-Item -ItemType Directory -Path $localDir -Force
+}
+
+
+$proxy="127.0.0.1:7890"
+# $proxy="192.168.255.1:7890"
+$env:HTTP_PROXY=$proxy; $env:HTTPS_PROXY=$proxy
+
+$syncList | ConvertFrom-Json | ForEach-Object {
+    if (-not (Test-Path -Path $localDir/$($_.archive))) {
+        $url="$($_.publicRegistry)/$($_.publicRepo)"
+        $localPath="$localDir/$($_.archive)"
+        Write-Host "Download $url to $localPath"
+        curl.exe --output $localPath $url
+    }
+}
+
+$proxy=""
+$env:HTTP_PROXY=$proxy; $env:HTTPS_PROXY=$proxy
+
+# curl -k -X MKCOL https://dufs.day0.sololab/binaries --user admin:admin
+
+$syncList | ConvertFrom-Json | ForEach-Object {
+    if (Test-Path -Path $localDir/$($_.archive)) {
+    $localPath="$localDir/$($_.archive)"
+    $url="$($_.privateRegistry)/$($_.privateRepo)"
+    $privateDir="$($_.privateRegistry)/$(Split-Path -Path $url -Parent)"
+    Write-Host "Create directory $privateDir if not exit"
+    curl -k -X MKCOL $privateDir --user admin:admin
+    Write-Host "Upload $localPath to $url"
+    curl -k -T $localPath $url --user admin:admin
+    ""
+    }
+    Start-Sleep -Seconds 1
+}
