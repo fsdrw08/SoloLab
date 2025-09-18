9593f99cddfc4276c8d355a926821b4ea7edd918
fsdrw08 <drw_08@hotmail.com>
2025-09-18 20:56:22 +0800

    lynx-ops: debug tf resource

--- TerraformWorkShop/Lynx/vyos/main.tf
+++ TerraformWorkShop/Lynx/vyos/main.tf
@@ -1,29 +1,41 @@
-resource "lynx_user" "user" {
-  for_each = {
-    for user in var.users : user.iac_id => user
+# resource "lynx_user" "user" {
+#   for_each = {
+#     for user in var.users : user.iac_id => user
+#   }
+#   name     = each.value.name
+#   email    = each.value.email
+#   role     = each.value.role
+#   password = each.value.password
+# }
+
+locals {
+  member_of = {
+    for user in var.users :
+    user.name => user.member_of
   }
-  name     = each.value.name
-  email    = each.value.email
-  role     = each.value.role
-  password = each.value.password
+  transpose = transpose(local.member_of)
 }
 
-resource "lynx_team" "monitoring" {
-  for_each = {
-    for team in var.teams : team.iac_id => team
-  }
-  name        = each.value.name
-  slug        = each.value.slug
-  description = each.value.description
-
-  members = [
-    for user in lynx_user
-    # lynx_user.stella.id,
-    # lynx_user.skylar.id,
-    # lynx_user.erika.id,
-    # lynx_user.adriana.id
-  ]
+output "debug" {
+  value = local.transpose
 }
+
+# resource "lynx_team" "monitoring" {
+#   for_each = {
+#     for team in var.teams : team.iac_id => team
+#   }
+#   name        = each.value.name
+#   slug        = each.value.slug
+#   description = each.value.description
+
+#   members = [
+#     for user in lynx_user
+#     # lynx_user.stella.id,
+#     # lynx_user.skylar.id,
+#     # lynx_user.erika.id,
+#     # lynx_user.adriana.id
+#   ]
+# }
 
 # resource "lynx_project" "grafana" {
 #   name        = "Grafana"
--- TerraformWorkShop/Lynx/vyos/terraform.tfvars
+++ TerraformWorkShop/Lynx/vyos/terraform.tfvars
@@ -2,3 +2,14 @@ prov_lynx = {
   api_url = "https://lynx.vyos.sololab"
   api_key = ""
 }
+
+users = [{
+  iac_id   = "123"
+  name     = "user1"
+  email    = "user1"
+  password = "user1"
+  member_of = [
+    "team1",
+    "team2"
+  ]
+}]
