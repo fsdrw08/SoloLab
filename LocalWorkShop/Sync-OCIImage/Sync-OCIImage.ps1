fa45070fad9544ece32b00c3c755e08a56032092
Windom.WU <drw_08@hotmail.com>
2025-09-22 18:01:17 +0800

    oci-ops: update oci sync script

--- LocalWorkShop/Sync-OCIImage/Sync-OCIImage.ps1
+++ LocalWorkShop/Sync-OCIImage/Sync-OCIImage.ps1
@@ -1,44 +1,112 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $SyncProfile,
 
-$repoDir = git rev-parse --show-toplevel
-$syncList = Get-Content -Path (Join-Path -Path $repoDir -ChildPath "LocalWorkShop\Sync-OCIImage\Day0.jsonc")
+    [Parameter()]
+    [string]
+    [ValidateSet(
+        'C:/Users/Public/Downloads/containers',
+        'D:/Users/Public/Downloads/containers'
+    )]
+    $LocalStore='C:/Users/Public/Downloads/containers',
+        
+    [Parameter()]
+    [bool]
+    [ValidateSet(
+        $true,
+        $false
+    )]
+    $Download=$true,
+        
+    [Parameter()]
+    [bool]
+    [ValidateSet(
+        $true,
+        $false
+    )]
+    $Upload=$true,
+
+    [ValidateNotNull()]
+    [System.Management.Automation.PSCredential]
+    [System.Management.Automation.Credential()]
+    $PrivateRegistryCredential = $(if ($Upload) {Get-Credential} else {$null})
+)
+
+if (!(Get-Command skopeo.exe)) {
+    "Download skopeo first"
+    exit 1
+}
+
+$syncList = Get-Content -Path (Join-Path -Path $PSScriptRoot -ChildPath $SyncProfile)
+# $repoDir = git rev-parse --show-toplevel
 # $syncList = Get-Content -Path (Join-Path -Path $repoDir -ChildPath "LocalWorkShop\Sync-OCIImage\VyOS.jsonc")
+# $syncList = Get-Content -Path (Join-Path -Path $repoDir -ChildPath "LocalWorkShop\Sync-OCIImage\Day0.jsonc")
 # $syncList = Get-Content -Path (Join-Path -Path $repoDir -ChildPath "LocalWorkShop\Sync-OCIImage\Day1.jsonc")
 # $syncList = Get-Content -Path (Join-Path -Path $repoDir -ChildPath "LocalWorkShop\Sync-OCIImage\Day2.jsonc")
 
-$localDir="$env:PUBLIC/Downloads/containers"
-# $localDir="C:/Users/Public/Downloads/containers"
-# $localDir="D:/Users/Public/Downloads/containers"
-if (-not (Test-Path -Path $localDir)) {
-    New-Item -ItemType Directory -Path $localDir -Force
+## validate json
+$referenceObject = @(
+    "publicRegistry"
+    "publicRepo"
+    "archive"
+    "privateRegistry"
+    "privateRepo"
+    )
+$syncList | ConvertFrom-Json | ForEach-Object {
+    $differenceObject = $_ | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty name 
+    $compareResult = Compare-Object -ReferenceObject $referenceObject -DifferenceObject $differenceObject
+    if ($compareResult -ne $null ) {
+        $resultDetail = $compareResult | Select-Object -ExpandProperty SideIndicator
+        if ($resultDetail -eq "<=") {
+            $_
+            $compareResult
+            Write-Host "validate the sync profile, <= means attribute missed, => means something not required, but it's fine to keep"
+            exit 1
+        }
+    }
+}
+
+# $LocalStore="$env:PUBLIC/Downloads/containers"
+# $LocalStore="C:/Users/Public/Downloads/containers"
+# $LocalStore="D:/Users/Public/Downloads/containers"
+if (-not (Test-Path -Path $LocalStore)) {
+    New-Item -ItemType Directory -Path $LocalStore -Force
 }
 
 
-$proxy="127.0.0.1:7890"
+## pull
+# $proxy="127.0.0.1:7890"
 # $proxy="192.168.255.1:7890"
-$env:HTTP_PROXY=$proxy; $env:HTTPS_PROXY=$proxy
-
-$syncList | ConvertFrom-Json | ForEach-Object {
-    if (-not (Test-Path -Path $localDir/$($_.archive))) {
-        Write-Host "Download docker image then convert to OCI archive $localDir/$($_.archive)"
-        skopeo copy --insecure-policy `
-            --override-os=linux `
-            --override-arch=amd64 `
-            docker://$($_.publicRegistry)/$($_.publicRepo) `
-            oci-archive:$localDir/$($_.archive)
+# $env:HTTP_PROXY=$proxy; $env:HTTPS_PROXY=$proxy
+if ($Download) {
+    $syncList | ConvertFrom-Json | ForEach-Object {
+        if (-not (Test-Path -Path $LocalStore/$($_.archive))) {
+            Write-Host "Download docker image then convert to OCI archive $LocalStore/$($_.archive)"
+            skopeo copy --insecure-policy `
+                --override-os=linux `
+                --override-arch=amd64 `
+                docker://$($_.publicRegistry)/$($_.publicRepo) `
+                oci-archive:$LocalStore/$($_.archive)
+        }
     }
 }
 
-$proxy=""
+## push
+$proxy=$null
 $env:HTTP_PROXY=$proxy; $env:HTTPS_PROXY=$proxy
-
-$syncList | ConvertFrom-Json | ForEach-Object {
-    if (Test-Path -Path $localDir/$($_.archive)) {
-    Write-Host "Upload OCI image OCI archive $localDir/$($_.archive) to $($_.privateRegistry)/$($_.privateRepo)"
-    skopeo copy --insecure-policy `
-        --dest-tls-verify=false `
-        --dest-creds="admin:P@ssw0rd" `
-        oci-archive:$localDir/$($_.archive) `
-        docker://$($_.privateRegistry)/$($_.privateRepo)
+if ($Upload) {
+    
+    $syncList | ConvertFrom-Json | ForEach-Object {
+        if (Test-Path -Path $LocalStore/$($_.archive)) {
+            Write-Host "Upload OCI image OCI archive $LocalStore/$($_.archive) to $($_.privateRegistry)/$($_.privateRepo)"
+            skopeo copy --insecure-policy `
+            --dest-tls-verify=false `
+            --dest-creds="$($PrivateRegistryCredential.UserName):$($PrivateRegistryCredential.GetNetworkCredential().Password)" `
+            oci-archive:$LocalStore/$($_.archive) `
+            docker://$($_.privateRegistry)/$($_.privateRepo)
+        }
+        Start-Sleep -Seconds 1
     }
-    Start-Sleep -Seconds 1
 }
--- LocalWorkShop/Sync-OCIImage/Sync-TrivyDB.ps1
+++ LocalWorkShop/Sync-OCIImage/Sync-TrivyDB.ps1
@@ -1,74 +1,135 @@
-# https://github.com/LubinLew/trivy-data-sync/blob/80befc585f54769cfd28cd28fc8d9e541ca4fbee/trivy_sync.sh#L112
-$syncList = @"
-[
-    {
-        "publicImage": "ghcr.io/aquasecurity/trivy-db:2",
-        "OCIFile": "db.tar.gz",
-        "OCIFileMediaType": "application/vnd.aquasec.trivy.db.layer.v1.tar+gzip",
-        "ManifestFile": "trivy-db-manifest.json",
-        "privateRepo": "aquasecurity/trivy-db:2"
-    },
-    {
-        "publicImage": "ghcr.io/aquasecurity/trivy-java-db:1",
-        "OCIFile": "javadb.tar.gz",
-        "OCIFileMediaType": "application/vnd.aquasec.trivy.javadb.layer.v1.tar+gzip",
-        "ManifestFile": "trivy-javadb-manifest.json",
-        "privateRepo": "aquasecurity/trivy-java-db:1"
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $SyncProfile,
+    
+    [Parameter()]
+    [ValidateSet(
+        'C:/Users/Public/Downloads/containers/trivy',
+        'D:/Users/Public/Downloads/containers/trivy'
+    )]
+    [string]
+    $LocalStore='C:/Users/Public/Downloads/containers/trivy',
+        
+    [Parameter()]
+    [bool]
+    [ValidateSet(
+        $true,
+        $false
+    )]
+    $Download=$true,
+        
+    [Parameter()]
+    [bool]
+    [ValidateSet(
+        $true,
+        $false
+    )]
+    $Upload=$true,
+        
+    [Parameter()]
+    [System.Management.Automation.PSCredential]
+    [System.Management.Automation.Credential()]
+    $PrivateRegistryCredential = $(if ($Upload) {Get-Credential} else {$null})
+)
+
+if (!(Get-Command oras.exe)) {
+    "Download oras first"
+    exit 1
+}
+
+$syncList = Get-Content -Path (Join-Path -Path $PSScriptRoot -ChildPath $SyncProfile)
+# $repoDir = git rev-parse --show-toplevel
+# $syncList = Get-Content -Path (Join-Path -Path $repoDir -ChildPath "LocalWorkShop\Sync-OCIImage\VyOS-Trivy.jsonc")
+# $syncList = Get-Content -Path (Join-Path -Path $repoDir -ChildPath "LocalWorkShop\Sync-OCIImage\Day0-Trivy.jsonc")
+
+## validate json
+$referenceObject = @(
+    "publicRegistry"
+    "publicRepo"
+    "archive"
+    "ociFileMediaType"
+    "privateRegistry"
+    "privateRepo"
+    "manifestFile"
+    )
+$syncList | ConvertFrom-Json | ForEach-Object {
+    $differenceObject = $_ | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty name 
+    $compareResult = Compare-Object -ReferenceObject $referenceObject -DifferenceObject $differenceObject
+    if ($compareResult -ne $null ) {
+        $resultDetail = $compareResult | Select-Object -ExpandProperty SideIndicator
+        if ($resultDetail -eq "<=") {
+            $_
+            $compareResult
+            Write-Host "validate the sync profile, <= means attribute missed, => means something not required, but it's fine to keep"
+            exit 1
+        }
     }
-]
-"@
+}
 
+# $LocalStore = "$env:PUBLIC/Downloads/containers/trivy"
+# $LocalStore="D:/Users/Public/Downloads/containers"
+if (-not (Test-Path $LocalStore)) {
+    New-Item -ItemType Directory -Path $LocalStore -Force
+}
 $currentLocation=Get-Location | Select-Object -ExpandProperty Path
-$localDir = "$env:PUBLIC/Downloads/containers/trivy"
-# $localDir="D:/Users/Public/Downloads/containers"
-# go to local oci archive dir
-if (-not (Test-Path $localDir)) {
-    New-Item -ItemType Directory -Path $localDir -Force
-}
-Set-Location -Path $localDir
 
-# set internet proxy for download
-$proxy="127.0.0.1:7890"
+
+## pull
+## https://github.com/LubinLew/trivy-data-sync/blob/80befc585f54769cfd28cd28fc8d9e541ca4fbee/trivy_sync.sh#L112
+# $proxy="127.0.0.1:7890"
 # $proxy="192.168.255.1:7890"
-$env:HTTP_PROXY=$proxy; $env:HTTPS_PROXY=$proxy
-
-# pull
-$syncList | ConvertFrom-Json | ForEach-Object {
-    oras pull $_.publicImage
-
-    # Download manifest
-    oras manifest fetch `
-    --output $_.ManifestFile `
-    $_.publicImage
+# $env:HTTP_PROXY=$proxy; $env:HTTPS_PROXY=$proxy
+if ($Download) {
+    $syncList | ConvertFrom-Json | ForEach-Object {
+        if (-not (Test-Path -Path $LocalStore/$($_.archive))) {
+            # go to local oci archive dir
+            Set-Location -Path $LocalStore
+            oras pull "$($_.publicRegistry)/$($_.publicRepo)"
+            
+            # Download manifest
+            oras manifest fetch `
+            --output $_.ManifestFile `
+            "$($_.publicRegistry)/$($_.publicRepo)"
+        }
+    }
 }
 
-# push
-$privateRegistry = "192.168.255.10:5000"
-# $privateRegistry = "zot.vyos.sololab.dev"
-$env:HTTP_PROXY=$null; $env:HTTPS_PROXY=$null
-# login to zot
-oras login --insecure -u admin $privateRegistry
-
-# Push the prior downloaded trivy-db to private registry
-# https://github.com/aquasecurity/trivy-db/blob/8c398f13db0ed9be333fe1b9ddab158ab7262967/README.md#building-the-db
-
-$trivyArtifactType = "application/vnd.aquasec.trivy.config.v1+json"
-$syncList | ConvertFrom-Json | ForEach-Object {
-    "oras push"
-    oras push `
-        --insecure `
-        --disable-path-validation `
-        --artifact-type $trivyArtifactType `
-        $privateRegistry/$($_.privateRepo) `
-        "$($_.OCIFile):$($_.OCIFileMediaType)"
-
-    "oras manifest push"
-    oras manifest push `
-        --insecure `
-        $privateRegistry/$($_.privateRepo) `
-        $_.ManifestFile
-
-    "oras manifest fetch"
-    oras manifest fetch --insecure $privateRegistry/$($_.privateRepo)
+## push
+$proxy=$null
+$env:HTTP_PROXY=$proxy; $env:HTTPS_PROXY=$proxy
+if ($Upload) {
+   
+    # Push the prior downloaded trivy-db to private registry
+    # https://github.com/aquasecurity/trivy-db/blob/8c398f13db0ed9be333fe1b9ddab158ab7262967/README.md#building-the-db
+    
+    $trivyArtifactType = "application/vnd.aquasec.trivy.config.v1+json"
+    $syncList | ConvertFrom-Json | ForEach-Object {
+        if (Test-Path -Path $LocalStore/$($_.archive)) {
+            "login to zot"
+            oras login --insecure `
+                --username $($PrivateRegistryCredential.UserName) `
+                --password $($PrivateRegistryCredential.GetNetworkCredential().Password) `
+                $_.privateRegistry
+            
+            "oras push"
+            oras push `
+                --insecure `
+                --disable-path-validation `
+                --artifact-type $trivyArtifactType `
+                "$($_.privateRegistry)/$($_.privateRepo)" `
+                "$($_.archive):$($_.ociFileMediaType)"
+            
+            "oras manifest push"
+            oras manifest push `
+                --insecure `
+                "$($_.privateRegistry)/$($_.privateRepo)" `
+                $_.manifestFile
+            
+            "oras manifest fetch"
+            oras manifest fetch --insecure "$($_.privateRegistry)/$($_.privateRepo)"
+        }
+    }
 }
 Set-Location -Path $currentLocation
