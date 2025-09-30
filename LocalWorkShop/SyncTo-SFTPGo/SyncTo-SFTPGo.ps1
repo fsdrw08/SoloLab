2fe30d9559a092e2f6fdc2d5e471b152d106fda4
Windom.WU <drw_08@hotmail.com>
2025-09-30 17:11:11 +0800

    sftpgo-ops: update script to upload files to sftpgo

--- LocalWorkShop/SyncTo-SFTPGo/SyncTo-SFTPGo.ps1
+++ LocalWorkShop/SyncTo-SFTPGo/SyncTo-SFTPGo.ps1
@@ -1,45 +1,121 @@
-$repoDir = git rev-parse --show-toplevel
-$syncList = Get-Content -Path (Join-Path -Path $repoDir -ChildPath "LocalWorkShop\SyncTo-Dufs\Day2.jsonc")
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $SyncProfile,
+
+    [Parameter()]
+    [string]
+    [ValidateSet(
+        'C:/Users/Public/Downloads/bin',
+        'D:/Users/Public/Downloads/bin'
+    )]
+    $LocalStore = 'C:/Users/Public/Downloads/bin',
+        
+    [Parameter()]
+    [bool]
+    [ValidateSet(
+        $true,
+        $false
+    )]
+    $Download=$true,
+        
+    [Parameter()]
+    [bool]
+    [ValidateSet(
+        $true,
+        $false
+    )]
+    $Upload=$true,
+
+    [Parameter()]
+    [string]
+    $PrivateRegistry = "https://sftpgo.day0.sololab",
+
+    [ValidateNotNull()]
+    [System.Management.Automation.PSCredential]
+    [System.Management.Automation.Credential()]
+    $PrivateRegistryCredential = $(if ($Upload) {Get-Credential} else {$null})
+)
+
+$syncList = Get-Content -Path (Join-Path -Path $PSScriptRoot -ChildPath $SyncProfile)
+# $repoDir = git rev-parse --show-toplevel
 # $syncList = Get-Content -Path (Join-Path -Path $repoDir -ChildPath "LocalWorkShop\SyncTo-Dufs\Day1.jsonc")
 
-$localDir="$env:PUBLIC/Downloads/bin"
-# $localDir="D:/Users/Public/Downloads/bin"
+## validate json
+$referenceObject = @(
+    "publicRegistry"
+    "publicRepo"
+    "archive"
+    "privateRepo"
+    )
+$syncList | ConvertFrom-Json | ForEach-Object {
+    $differenceObject = $_ | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty name 
+    $compareResult = Compare-Object -ReferenceObject $referenceObject -DifferenceObject $differenceObject
+    if ($compareResult -ne $null ) {
+        $resultDetail = $compareResult | Select-Object -ExpandProperty SideIndicator
+        if ($resultDetail -eq "<=") {
+            $_
+            $compareResult
+            Write-Host "validate the sync profile, <= means attribute missed, => means something not required, but it's fine to keep"
+            exit 1
+        }
+    }
+}
+
+# $LocalStore="$env:PUBLIC/Downloads/containers"
 # $localDir="C:/Users/Public/Downloads/containers"
 # $localDir="D:/Users/Public/Downloads/containers"
-if (-not (Test-Path -Path $localDir)) {
-    New-Item -ItemType Directory -Path $localDir -Force
+if (-not (Test-Path -Path $LocalStore)) {
+    New-Item -ItemType Directory -Path $LocalStore -Force
 }
 
 
+## download
 $proxy="127.0.0.1:7890"
 # $proxy="192.168.255.1:7890"
 $env:HTTP_PROXY=$proxy; $env:HTTPS_PROXY=$proxy
-
-$syncList | ConvertFrom-Json | ForEach-Object {
-    if ((-not (Test-Path -Path $localDir/$($_.archive))) -and $_.publicRegistry ) {
-        $url="$($_.publicRegistry)/$($_.publicRepo)"
-        $localPath="$localDir/$($_.archive)"
-        Write-Host "Download $url to $localPath"
-        curl.exe --output $localPath $url
+if ($Download) {
+    $syncList | ConvertFrom-Json | ForEach-Object {
+        if ((-not (Test-Path -Path $LocalStore/$($_.archive))) -and $_.publicRegistry ) {
+            $url="$($_.publicRegistry)/$($_.publicRepo)"
+            $localPath="$LocalStore/$($_.archive)"
+            Write-Host "Download $url to $localPath"
+            curl.exe --output $localPath $url
+        }
     }
 }
 
+## upload
 $proxy=""
 $env:HTTP_PROXY=$proxy; $env:HTTPS_PROXY=$proxy
 
-# curl -k -X MKCOL https://dufs.day0.sololab/binaries --user admin:admin
-$credential="admin:P@ssw0rd"
-
-$syncList | ConvertFrom-Json | ForEach-Object {
-    if ($_.archive -and (Test-Path -Path $localDir/$($_.archive))) {
-        $localPath="$localDir/$($_.archive)"
-        $url="$($_.privateRegistry)/$($_.privateRepo)"
-        $privateDir="$($_.privateRegistry)/$(Split-Path -Path $_.privateRepo -Parent)"
-        Write-Host "Create directory $privateDir if not exit"
-        curl.exe -k -X MKCOL $privateDir --user $credential
-        Write-Host "Upload $localPath to $url"
-        curl.exe -k -T $localPath $url --user $credential
-        ""
+$credential="$($PrivateRegistryCredential.UserName):$($PrivateRegistryCredential.GetNetworkCredential().Password)"
+if ($Upload) {
+    Write-Host "Login ..."
+    $token = curl -s --request GET `
+        -k --url "$PrivateRegistry/api/v2/user/token" `
+        --header "Authorization: Basic $([Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes($credential)))" `
+        --header "Accept: application/json" | ConvertFrom-Json | Select-Object -ExpandProperty access_token
+    $syncList | ConvertFrom-Json | ForEach-Object {
+        if ($_.archive -and (Test-Path -Path $LocalStore/$($_.archive))) {
+            $localPath="$LocalStore/$($_.archive)"
+            $encodedString = [System.Net.WebUtility]::UrlEncode($_.privateRepo)
+            # https://sftpgo.stoplight.io/docs/sftpgo/seqob1os7tkjh-upload-a-single-file
+            $url="$($PrivateRegistry)/api/v2/user/files/upload?mkdir_parents=true&path=$encodedString"
+            Write-Host "Upload $localPath to $url"
+            curl.exe --request POST `
+                -k --url $url `
+                --header 'Accept: application/json' `
+                --header "Authorization: Bearer $token" `
+                --upload-file $localPath
+            ""
+        }
+        Start-Sleep -Seconds 1
     }
-    Start-Sleep -Seconds 1
+    Write-Host "Logout ..."
+    curl.exe --request GET `
+        -k --url "$PrivateRegistry/api/v2/user/logout" `
+        --header 'Accept: application/json' `
+        --header "Authorization: Bearer $token"
 }
