54487724ced458321eec57a2f92f54c37da771b7
Windom.WU <drw_08@hotmail.com>
2025-11-17 18:13:57 +0800

    nfs-controller-deploy-nomad-job: correct job name

--- TerraformWorkShop/Nomad/Jobs/nfs-controller/main.tf
+++ TerraformWorkShop/Nomad/Jobs/nfs-controller/main.tf
@@ -1,5 +1,5 @@
 # https://github.com/jbaikge/homelab-nomad/blob/ce67445a95aa7dd5c2e5d72b11e06b078e44e67c/nomad/traefik.tf#L2
-resource "nomad_job" "traefik" {
+resource "nomad_job" "nfs-controller" {
   jobspec = file("${path.module}/attachments/nfs-controller.nomad.hcl")
 
   hcl2 {
