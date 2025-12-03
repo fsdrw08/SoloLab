345f093b9066027a715f00bb229520f6e63766fa
Windom.WU <drw_08@hotmail.com>
2025-12-03 17:56:02 +0800

    traefik-deploy-nomad-job: fix - update proxy protocol settings and terraform backend
    - Add trusted IPs for proxy protocol in traefik configuration
    - Switch terraform backend from s3 to consul for state management

--- TerraformWorkShop/Nomad/Jobs/traefik/provider.tf
+++ TerraformWorkShop/Nomad/Jobs/traefik/provider.tf
@@ -5,24 +5,10 @@ terraform {
       version = ">= 2.5.1"
     }
   }
-  backend "s3" {
-    bucket = "tfstate"           # Name of the S3 bucket
-    key    = "Nomad/Job-Traefik" # Name of the tfstate file
-
-    endpoints = {
-      s3 = "https://minio-api.day1.sololab" # Minio endpoint
-    }
-
-    access_key = "terraform" # Access and secret keys
-    secret_key = "terraform"
-
-    region                      = "main" # Region validation will be skipped
-    skip_credentials_validation = true   # Skip AWS related checks and validations
-    skip_metadata_api_check     = true
-    skip_region_validation      = true
-    use_path_style              = true
-    skip_requesting_account_id  = true
-    insecure                    = true
+  backend "consul" {
+    address = "consul.day1.sololab"
+    scheme  = "https"
+    path    = "tfstate/Nomad/Job-Traefik"
   }
 }
 
