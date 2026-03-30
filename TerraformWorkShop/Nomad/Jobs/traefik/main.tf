492fd6b36014af077d52523ae9e46b89876e7829
Windom.WU <drw_08@hotmail.com>
2026-03-30 18:01:27 +0800

    traefik-deploy: nomad-job: improve variable structure day1-podman-kube: enlarge resource

--- TerraformWorkShop/Nomad/Jobs/traefik/main.tf
+++ TerraformWorkShop/Nomad/Jobs/traefik/main.tf
@@ -1,15 +1,26 @@
+locals {
+  job_vars_map = {
+    for job in var.jobs : job.path => {
+      vars = job.var_sets == null ? {} : {
+        for var_set in job.var_sets :
+        "${var_set.name}" => var_set.value_string == null ? templatefile(var_set.value_template_path, var_set.value_template_vars) : var_set.value_string
+      }
+    }
+  }
+}
+
 # https://github.com/jbaikge/homelab-nomad/blob/ce67445a95aa7dd5c2e5d72b11e06b078e44e67c/nomad/traefik.tf#L2
-resource "nomad_job" "job" {
-  jobspec = file("${path.module}/attachments/traefik.nomad.hcl")
+resource "nomad_job" "jobs" {
+  for_each = {
+    for job in var.jobs : job.path => job
+  }
+
+  jobspec = file(each.key)
 
   hcl2 {
     allow_fs = true
-    vars = {
-      install_config = file("${path.module}/attachments/install.traefik.yml")
-      routing_config = file("${path.module}/attachments/routing.traefik.yml")
-    }
+    vars     = local.job_vars_map[each.key] == null ? {} : local.job_vars_map[each.key].vars
   }
 
   purge_on_destroy = true
 }
-
--- TerraformWorkShop/Nomad/Jobs/traefik/terraform.tfvars
+++ TerraformWorkShop/Nomad/Jobs/traefik/terraform.tfvars
@@ -2,3 +2,19 @@ prov_nomad = {
   address     = "https://nomad.day1.sololab"
   skip_verify = true
 }
+
+jobs = [
+  {
+    path = "./attachments/traefik.nomad.hcl"
+    var_sets = [
+      {
+        name                = "install_config"
+        value_template_path = "./attachments/install.traefik.yml"
+      },
+      {
+        name                = "routing_config"
+        value_template_path = "./attachments/routing.traefik.yml"
+      }
+    ]
+  },
+]
