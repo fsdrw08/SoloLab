80cfca99531e18735290d8601477f6abaf648772
Windom.WU <drw_08@hotmail.com>
2026-05-05 10:36:44 +0800

    nexus-ops-repo: add docker repo

---
+++ TerraformWorkShop/Nexus/Repository/Docker/main.tf
@@ -0,0 +1,36 @@
+resource "nexus_repository_docker_proxy" "docker_proxy" {
+  name   = "dockerhub"
+  online = true
+
+  docker {
+    force_basic_auth = false
+    v1_enabled       = false
+  }
+
+  docker_proxy {
+    index_type = "HUB"
+  }
+
+  storage {
+    blob_store_name                = "minio.day0"
+    strict_content_type_validation = true
+  }
+
+  proxy {
+    remote_url       = "https://registry-1.docker.io"
+    content_max_age  = 1440
+    metadata_max_age = 1440
+  }
+
+  negative_cache {
+    enabled = true
+    ttl     = 1440
+  }
+
+  http_client {
+    blocked    = false
+    auto_block = true
+  }
+
+  cache_foreign_layers = true
+}
